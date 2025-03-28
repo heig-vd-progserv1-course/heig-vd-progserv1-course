a7297d76e62209f02396986288d7efee14017398
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2025-03-28 08:58:23 +0000

    Séparation du fichier 'view.php' en 'view.php' et 'update.php'

--- 02-fonctions/02-mini-project/solution/progserv1/mini-projet/view.php
+++ 02-fonctions/02-mini-project/solution/progserv1/mini-projet/view.php
@@ -2,13 +2,13 @@
 <html lang="fr">
 
 <head>
-    <title>Visualise et modifie un animal de compagnie | Gestionnaire d'animaux de compagnie</title>
+    <title>Visualise un animal de compagnie | Gestionnaire d'animaux de compagnie</title>
 </head>
 
 <body>
-    <h1>Visualise et modifie un animal de compagnie</h1>
+    <h1>Visualise un animal de compagnie</h1>
     <p><a href="index.php">Retour à l'accueil</a></p>
-    <p>Utilise cette page pour visualiser et modifier un animal de compagnie.</p>
+    <p>Utilise cette page pour visualiser un animal de compagnie.</p>
 </body>
 
 </html>
